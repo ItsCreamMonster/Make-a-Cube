d2e9276f19252b3a872957f68d1e541fb103a510
ItsCreamMonster <125822771+ItsCreamMonster@users.noreply.github.com>
2023-03-22 16:43:56 +0100

    [v1.0.1] Added code output and fixes + changes.

--- script.js
+++ script.js
@@ -95,11 +95,14 @@ var lightBtn = document.querySelector(".lightActivated-btn");
 
 var allText = document.querySelectorAll("h1 h2 h3 h4 h5 p");
 
+var codeBtn = document.querySelector(".sourceCode-btn");
+
 var h1 = document.querySelector("h1");
 var h2 = document.querySelector("h2");
 var h3 = document.querySelector("h3");
 var h4 = document.querySelector("h4");
 var p = document.querySelector("p");
+var label = document.querySelectorAll("label");
 
 function setLightMode() {
     console.log('Set light mode successfully!')
@@ -107,7 +110,19 @@ function setLightMode() {
     darkBtn.style.display = "none";
     lightBtn.style.display = "";
 
-    allText.style.color = "rgb(0,0,0)"
+    // allText.style.color = "rgb(0,0,0)"
+    h1.style.color = "#000000"
+    h2.style.color = "#000000"
+    h3.style.color = "#000000"
+    
+    for(var h4Items = 0; h4Items < h4.length; h4Items++){
+        h4.style.color = "#000000"    
+    }
+    codeBtn.style.border = "none"
+
+    for(var labelItems = 0; labelItems < label.length; labelItems++){
+        label[labelItems].style.color = "#000000"
+    }
 }
 
 function setDarkMode() {
@@ -116,10 +131,19 @@ function setDarkMode() {
     darkBtn.style.display = ""
     lightBtn.style.display = "none"
 
+    
+
     h1.style.color = "#ffffff"
     h2.style.color = "#ffffff"
     h3.style.color = "#ffffff"
+    for(var h4Items = 0; h4Items < h4.length; h4Items++){
     h4.style.color = "#ffffff"
+    }
+    codeBtn.style.border = "3px solid #f0f0f0"
+
+    for(var labelItems = 0; labelItems < label.length; labelItems++){
+        label[labelItems].style.color = "#ffffff"
+    }
 }
 
 
@@ -136,3 +160,37 @@ document.cookie = "color=" + colorInput.value + "; expires=";
 } */
 
     
+// HIGHLIGHT.JS
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('pre code').forEach((el) => {
+      hljs.highlightElement(el);
+    });
+});
+
+
+
+// EMBEDDING
+    
+function setOutputCode() {
+    var outputCode = document.querySelector("#outputCode").innerHTML;
+    
+    outputCode.innerHTML = "/* Make-a-Cube */ \n \n #cube {\n     width: 200px; \n    height: 200px; \n \n    background-color: " + colorInput.value + "; \n     border-radius: " + borderRadiusInput.value + "; \n     border: " + borderInput.value + "; \n     box-shadow: " + boxShadowInput.value + "; \n \n     cursor: " + cursorInput.value + "; \n     transition: " + transitionInput.value + "; \n }"
+}
+
+    // Get HTML code
+function copyOutput() {
+    navigator.clipboard.writeText("<div id='cube'></div> \n <style> \n     /* Make-a-Cube */ \n \n     #cube {\n         width: 200px; \n        height: 200px; \n \n        background-color: " + colorInput.value + "; \n         border-radius: " + borderRadiusInput.value + "; \n         border: " + borderInput.value + "; \n         box-shadow: " + boxShadowInput.value + "; \n \n         cursor: " + cursorInput.value + "; \n         transition: " + transitionInput.value + "; \n } \n </style>")
+
+    document.getElementById("copySuccess").style.visibility = "visible";
+
+    /* if (borderRadiusInput.value && borderInput.value && boxShadowInput == "") {
+        document.getElementById("copyDisclaimer").style.visibility = "visible";
+        document.getElementById("copySuccess").style.visibility = "visible";
+
+        document.getElementById("copySuccess").style.color = "#ff0000";
+        document.getElementById("copySuccess").style.backgroundColor = "#ff000031"
+    } */
+    
+}
+
